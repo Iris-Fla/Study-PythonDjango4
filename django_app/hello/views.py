333218295ac3e9106f3644a0a29b65a422a2750e
Iris-Fla <irith.fla@gmail.com>
2024-05-14 12:40:26 +0900

    0514

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import HelloForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'hello/index.html')
+    params = {
+        "title":"Hello",
+        "message":"your data:",
+        "form":HelloForm()
+    }
+    if (request.method == "POST"):
+        params["message"] = "名前:" + request.POST["name"] + "<br>メール:" + request.POST["mail"] + "<br>年齢:" + request.POST["age"]
+        params["form"] = HelloForm(request.POST)
+    return render(request,"hello/index.html",params)
