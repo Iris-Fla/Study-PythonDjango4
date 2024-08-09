3c863555038d5513dea845a5c89a982714c66007
Meru🔮 <irith.fla@gmail.com>
2024-08-09 18:57:32 +0900

    3章終了

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -3,7 +3,15 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
 from .models import Friend
-from .forms import FriendForm,HelloForm
+from .forms import FriendForm,HelloForm,FindForm
+from django.views.generic import DetailView, ListView
+
+
+class FriendList(ListView):
+    model = Friend
+
+class FriendDetail(DetailView):
+    model = Friend
 
 # Create your views here.
 def index(request):
@@ -47,3 +55,33 @@ def edit(request,num):
         'form':FriendForm(instance=obj),
     }
     return render(request, 'hello/edit.html', params)
+
+def delete(request,num):
+    friend = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend.delete()
+        return redirect(to='/hello')
+    params = {
+        'title':'Hello',
+        'id':num,
+        'obj':friend,
+    }
+    return render(request, 'hello/delete.html', params)
+
+def find(request):
+    if (request.method == 'POST'):
+        form = FindForm(request.POST)
+        find = request.POST['find']
+        data = Friend.objects.filter(name__contains=find)
+        msg = "result:" + str(data.count())
+    else:
+        msg = 'search words...'
+        form = FindForm()
+        data = Friend.objects.all()
+    params = {
+        'title':'Hello',
+        'message':msg,
+        'form':form,
+        'data':data,
+    }
+    return render(request, 'hello/find.html', params)
